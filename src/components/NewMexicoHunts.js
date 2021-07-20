70f117708f3c4572b101536db4fdf04ef25e7109
kurwitz3 <kurwitz3@aol.com>
2021-07-19 22:44:29 -0400

    css changes

--- src/components/NewMexicoHunts.js
+++ src/components/NewMexicoHunts.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import '../CSS/Hunts.css'
 
 const NewMexicoHunts = (props) => {
    return (
         <div className='card'>
           <div>
-            <h2 id='animal'>{props.hunt.animal}</h2>
+            <h2 className='animal'>{props.hunt.animal}</h2>
             <p className="desc">{props.hunt.description}</p>
             <Link to='/Rates' className="btn">Rates and Info</Link>
           </div>
--- src/components/Rates.js
+++ src/components/Rates.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../CSS/Rates.css'
 
 
 const Rates = (props) => {
