6e47666cb9a4ae6bfa9b533f64ad5aee50c450cd
kurwitz3 <kurwitz3@aol.com>
2021-07-21 11:35:56 -0400

    final commit clean up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
-import './App.css';
+import './CSS/App.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import HuntsContainer from './containers/HuntsContainer';
--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './CSS/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux'
 import {createStore,applyMiddleware} from 'redux'
 import thunk from 'redux-thunk';
 import manageHunts from './reducers/manageHunts'
-
-
 
 const store = createStore(manageHunts,applyMiddleware(thunk))
 
